30b3f99900fa7d0afdd68d396beed5b1cbc50716
pqud <49116761+pqud@users.noreply.github.com>
2025-06-06 23:52:17 +0900

    feat: anon-swap 하는중

--- pintos-kaist/vm/vm.c
+++ pintos-kaist/vm/vm.c
@@ -150,7 +150,7 @@ vm_get_victim(void)
 	struct frame *victim;
 	/* TODO: 교체 정책을 여기서 구현해서 희생자 페이지 찾기 */
 
-	victim= list_pop_front(&frame_table->frame_list);
+	victim= list_entry(list_pop_front(&frame_table->frame_list), struct frame, frame_elem);
 	ASSERT(victim!=NULL);
 
 	return victim;
@@ -167,7 +167,7 @@ vm_evict_frame(void)
 		if (!swap_out(page))
 			return NULL;
 		pml4_clear_page(&thread_current()->pml4, page->va);
-		list_remove(&page->frame->frame_elem);
+		// list_remove(&page->frame->frame_elem);
 
 		page->frame = NULL; // 연결 해제
 
